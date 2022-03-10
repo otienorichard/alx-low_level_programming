a509a22152ee5546bd407a03517613ab7814c05c
Richard otieno <99313847+otienorichard@users.noreply.github.com>
2022-03-10 22:18:14 +0300

    new

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-int main()
-/**
- * main-main block
- * Return: 0
- */
+int main(void)
+    /**
+     * main-main block
+    * Return: 0
+    */
 {
     puts("\"Programming is like building a multilingual puzzle");
     return 0;
