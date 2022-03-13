9689f221aa70edcd0cbc8f68c3379e35cadcf6e6
Richard otieno <99313847+otienorichard@users.noreply.github.com>
2022-03-13 20:57:17 +0300

    changes made

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
-*main - prints the alphabet in lower case, except q and e
+*main - prints the alphabet in lowercase, except q and e
 *followed by a new line,
 Return: Always 0 (Success)
 */
