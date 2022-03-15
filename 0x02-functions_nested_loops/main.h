dc3dbab27bad62144c1dfadef7fde9ce775ab2e6
Richard otieno <99313847+otienorichard@users.noreply.github.com>
2022-03-15 19:04:34 +0300

    new

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,18 +1,1 @@
-#ifndef_main_h_
-#define_main_h_
-
-int_putchar(char);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int_islower(int c);
-int_isalpha(int c);
-int print_sign(int n);
-int_abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int,int);
-void print_to_98(int n);
-void print_times_table(int n);
-
-#endif
+define CHAR "_putchar"
