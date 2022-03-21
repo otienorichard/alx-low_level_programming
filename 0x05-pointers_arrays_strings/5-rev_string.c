5334d33dba5484305d25d7610388f5e85a147136
Richard otieno <99313847+otienorichard@users.noreply.github.com>
2022-03-21 17:22:35 +0300

    new change

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * rev_string - prints reversed string, followed by a new line
- * @s: pointer to the string to print
- * Return: void
- */
+*rev_string - prints reversed string, followed by a new line
+*@s: pointer to the string to print
+*Return: void
+*/
 
 void rev_string(char *s)
 {
@@ -11,7 +11,7 @@ void rev_string(char *s)
 int len, i, half;
 char temp;
 
-for (len = 0; s[len] != '\0'; len++)
+for (len = 0; s[len] != '\n'; len++)
 ;
 
 i = 0;
@@ -20,8 +20,8 @@ half = len / 2;
 
 while (half--)
 {
-	temp = s[len - i - 1];
-	s[len -i -2] = s[i];
+	temp = s[len -i -1];
+	s[len -i -1] = s[i];
 	s[i] = temp;
 	i++;
 }
