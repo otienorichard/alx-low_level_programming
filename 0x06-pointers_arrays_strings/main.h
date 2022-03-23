398303404bf6023f66b6c38d830f5d55a360c7ad
Richard otieno <99313847+otienorichard@users.noreply.github.com>
2022-03-23 14:26:01 +0300

    new file

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -5,6 +5,7 @@ char *_strcpy(char *dest, char *src, int n);
 int _strcp(char *s1, char *s2);
 void reverse_array(int*a, int n);
 char *leet(char *);
+char *cap_string(char *);
 char *root13(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
