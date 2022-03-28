08fa8ce7459310cf6cb4cbea79ea7153dc504f54
Richard otieno <99313847+otienorichard@users.noreply.github.com>
2022-03-28 22:56:21 +0300

    new file

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _memset - files memory with a constant byte,
+ * @S: memort area,
+ * @b: constant byte.
+ * @n: bytes filled.
+ * Return: the pointer to dest.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		*(s + i) =  b;
+
+	return (s);
+}
