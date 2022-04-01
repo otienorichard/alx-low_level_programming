468d2655cfdf49fc4b6999ec7194a0c72260b417
Richard otieno <99313847+otienorichard@users.noreply.github.com>
2022-04-02 01:31:17 +0300

    new corrected files,will they work!

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+* _slower - check for lower case letter
+* @c : character to check the case
+* Return:0 if lowercase. 1 if uppercase
+*/
+int _islower(int c)
+{
+        return (c >= 97 && c <= 122);
+}
