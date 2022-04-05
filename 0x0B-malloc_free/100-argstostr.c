465ae27c5b3cbb0bbd03a70fe06c58efcf2bd03e
Richard otieno <99313847+otienorichard@users.noreply.github.com>
2022-04-05 16:36:22 +0300

    new

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -40,8 +40,8 @@ if (s == 0)
 
 for (i = 0; i < ac; i++)
 {
-for (j = 0; av[i][j] != '\0'; j++, cmpt++)
-	s[cmpt] = av[i][j];
+	for (j = 0; av[i][j] != '\0'; j++, cmpt++)
+		s[cmpt] = av[i][j];
 
 	s[cmpt] = '\n';
 		cmpt++;
