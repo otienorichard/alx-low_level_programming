28473ebaff95207046b28c980b2aed5047d4e6c6
Richard otieno <99313847+otienorichard@users.noreply.github.com>
2022-04-05 15:04:48 +0300

    new file

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -14,8 +14,8 @@ char *_strdup(char *str)
 	int i = 0, size = 0;
 	char *m;
 
-	if (str == NULL)
-			return (NULL);
+if (str == NULL)
+	return (NULL);
 
 	for (; str[size] != '\0'; size++)
 		;
@@ -23,8 +23,8 @@ char *_strdup(char *str)
 	/*+1 on the size puts the end of string character*/
 	m = malloc(size * sizeof(*str) + 1);
 
-	if (m == 0)
-			return (NULL);
+if (m == 0)
+	return (NULL);
 	else
 	{
 		for (; i < size; i++)
