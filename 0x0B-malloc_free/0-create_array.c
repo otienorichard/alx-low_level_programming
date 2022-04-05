7892c0ab727278818c6464b9915f7cfe07466e1c
Richard otieno <99313847+otienorichard@users.noreply.github.com>
2022-04-05 14:52:00 +0300

    modification of the file

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,10 +13,10 @@ char *create_array(unsigned int size, char c)
 	char *n = malloc(size);
 
 	if (size == 0 || n == 0)
-		return(0);
+		return (0);
 
 	while (size--)
 		n[size] = c;
 
-	return(n);
+	return (n);
 }
