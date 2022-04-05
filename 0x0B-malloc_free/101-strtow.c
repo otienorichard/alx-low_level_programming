ae977d80bb4a86ea1b93fdcb0e00350b3921fb74
Richard otieno <99313847+otienorichard@users.noreply.github.com>
2022-04-05 17:14:44 +0300

    compilation of the file

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,13 +1,12 @@
 #include "main.h"
 #include <stdlib.h>
+
 /**
  * wrdcnt - counts the number of words in a string
  * @s: string to count
  *
  * Return: int of number of words
  */
-
-
 
 
 int wrdcnt(char *s)
